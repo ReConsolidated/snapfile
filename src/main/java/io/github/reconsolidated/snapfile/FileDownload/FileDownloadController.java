51152cdf7ed67e6a7a2ab9b3bdc95cfa0f40c4e1
ReConsolidated <gracjanpasik@gmail.com>
2022-11-06 01:51:15 +0100

    import

--- src/main/java/io/github/reconsolidated/snapfile/FileDownload/FileDownloadController.java
+++ src/main/java/io/github/reconsolidated/snapfile/FileDownload/FileDownloadController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.util.Optional;
 
